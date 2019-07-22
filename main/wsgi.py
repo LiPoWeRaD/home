58078155c32ef43f369373b69ea4097b98559e5e
LiPoWeRaD <limon98@list.ru>
2019-07-22 03:38:32 +0300

    fix15

--- main/wsgi.py
+++ main/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
+path = '/home/LiPoWeRaD/lipowerad.pythonanywhere.com'
+if path not in sys.path:
+    sys.path.append(path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+os.environ['DB_PWD'] = 'tujh6966'
 
 application = get_wsgi_application()
--- one/models.py
+++ one/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Perf(models.Model):
+    id = models.AutoField(primary_key=True)
     Имя = models.CharField(max_length=10, db_column='name')
     Логин = models.CharField(max_length=12, unique=True, db_column='login')
     Пароль = models.IntegerField(db_column='password')
 class Stat(models.Model):
-    Логин = models.ForeignKey(Perf, on_delete=models.CASCADE, db_column='login')
+    Логин = models.ForeignKey(Perf, on_delete=models.CASCADE)
     Название = models.CharField(max_length=25, db_column='name')
     Информация = models.TextField(db_column='info')
     Дата = models.DateField(db_column='Date')
